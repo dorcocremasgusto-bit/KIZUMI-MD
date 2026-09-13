bc5e5c35e85659be548db816994ab0873c171094
dorcocremasgusto-bit <dorcocremasgusto@gmail.com>
2026-09-13 10:07:52 -0400

    Update ninjaStyle.js

--- utils/ninjaStyle.js
+++ utils/ninjaStyle.js
@@ -48,7 +48,7 @@ function box(title, lines = []) {
  */
 function reply(title, lines = []) {
     const content = Array.isArray(lines) ? lines : [lines];
-    const text = `${TOP_BAR}\n${TOP_SUB}\n\n${box(title, content)}\n\n${RULE_LONG}\n${BOTTOM_BAR}\n⚔︎ Dev : ${Arthur Dev}\n└───────────⊳`;
+    const text = `${TOP_BAR}\n${TOP_SUB}\n\n${box(title, content)}\n\n${RULE_LONG}\n${BOTTOM_BAR}\n⚔︎ Dev : ${DEV_NAME}\n└───────────⊳`;
     return mono(text);
 }
 
